6267ae78368662e5e1b5babfc9b58c38d29a7673
Nishant Kumar <work.nishantkumar@gmail.com>
2023-06-17 01:36:11 +0530

    addinf .env & mongoDB insert

--- models/todo.models.js
+++ models/todo.models.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const collection = client.db('Todo_api').collection('todo');
 const todoSchema=new mongoose.Schema({
   title:{
     type:String,
@@ -18,3 +19,39 @@ const Todo = mongoose.model("Todo",todoSchema)
 module.exports = {
   Todo,
 }
+collection.insertOne(document, (err, result) => {
+    if (err) {
+      console.error('Error inserting document:', err);
+      return;
+    }
+    
+    console.log('Document inserted successfully:', result.insertedId);
+  });
+
+
+  collection.insertMany(documents, (err, result) => {
+    if (err) {
+      console.error('Error inserting documents:', err);
+      return;
+    }
+    
+    console.log('Documents inserted successfully:', result.insertedCount);
+  });
+
+
+  collection.deleteOne(filter, (err, result) => {
+    if (err) {
+      console.error('Error deleting document:', err);
+      return;
+    }
+    
+    console.log('Document deleted successfully');
+  });
+  collection.deleteMany(filter, (err, result) => {
+    if (err) {
+      console.error('Error deleting documents:', err);
+      return;
+    }
+    
+    console.log('Documents deleted successfully:', result.deletedCount);
+  });
